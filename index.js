609a66aa25b20de909449bd242bbef1a2b02b437
mbautista95 <mbautistarubiano@gmail.com>
2024-04-19 21:34:20 -0500

    Código actualizado con gitignoreincluyendo el archivo .env que tiene la clave hacia mi API privada

--- index.js
+++ index.js
@@ -1,49 +1,44 @@
 require('dotenv').config();
 
-
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors'); 
 const mongoString = process.env.DATABASE_URL;
 const routeDirector = require('./routes/routeDirector');
 const routeGenero = require('./routes/routeGenero');
 const routeMedia = require('./routes/routeMedia');
 const routeProductora = require('./routes/routeProductora');
 const routeTipo = require('./routes/routeTipo');
- 
+
 mongoose.connect(mongoString);
+
 const database = mongoose.connection;
 
 
 database.on('error', (error) => {
-    
-    console.log(error)
-})
-
+  console.log(error);
+});
 
 database.once('connected', () => {
-    console.log('Database Connected');
-
-
-})
+  console.log('Database Connected');
+});
 
 const app = express();
 
+// Add the cors middleware early in your middleware chain
+app.use(cors()); // Allow requests from any origin (for development)
+
 app.use(express.json());
 
-
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
-})
-
+app.listen(4000, () => {
+  console.log(`Server Started at ${4000}`);
+});
 
 
 app.use('/directores', routeDirector);
 app.use('/generos', routeGenero);
-app.use('/MediaItems',  routeMedia);
-app.use('/Productoras', routeProductora)
-app.use('/Tipos', routeTipo)
-
-
-
+app.use('/MediaItems', routeMedia);
+app.use('/Productoras', routeProductora);
+app.use('/Tipos', routeTipo);
 
 
